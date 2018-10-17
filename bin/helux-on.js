373c9b4aad69204abde8cbd7089fc9cbf7e74373
dauta <idauta@gmail.com>
2018-10-17 15:09:38 +0200

    got rid of broken dependency and fixed geolocation search

--- bin/helux-on.js
+++ bin/helux-on.js
@@ -1,7 +1,6 @@
 const cp = require('child_process');
-const path = require('path');
 const fs = require('fs');
-const geocoder = require('geocoder');
+const { get } = require('axios');
 //location taken from arguments list
 const location = process.argv[2];
 //check the input
@@ -10,16 +9,11 @@ if(!location){
   process.exit(1);
 }
 //geocode
-geocoder.geocode(location, (err, geo)=>{
-  if(geo.results.length === 0){
-    console.log(`ERROR: Could not locate "${location}". Please, try again.`);
-    return;
-  }
-  //address
-  const address = geo.results[0].formatted_address;
-  //coordinates
-  const lat = geo.results[0].geometry.location.lat;
-  const lon = geo.results[0].geometry.location.lng;
+const geocode = async (location) => {
+  const result = 
+    await get(`https://nominatim.openstreetmap.org/search/${location}?format=json&limit=1`);
+
+  const { lat, lon, display_name } = result.data[0];
   //execute xflux with extracted coordinates
   let xflx = cp.spawn(__dirname + '/xflux', ['-l', lat, '-g', lon], {detached: true, stdio: ['ignore', 'pipe', 'pipe']});
   //catch the output
@@ -28,9 +22,12 @@ geocoder.geocode(location, (err, geo)=>{
       //detach child from this process
       xflx.unref();
       //display the output
-      console.log('Helux is on\nCurrent location:', address);
+      console.log('Enjoy using Helux!\nCurrent location is ' + display_name);
       //kill the parent process
       process.exit(0);
     });
-  })
-})
+  });
+}
+
+//launch
+geocode(location);
